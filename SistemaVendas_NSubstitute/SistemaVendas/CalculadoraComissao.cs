da8bdce5ffe846bf3a8e6e77a4b225c11aa0f452
cursoTestes <cfc@ufrj.br>
2016-05-19 16:33:13 +0100

    Bota fogo

---
+++ SistemaVendas_NSubstitute/SistemaVendas/CalculadoraComissao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemaVendas
+{
+    public class CalculadoraComissao
+    {
+
+
+        
+
+        public decimal calcular(decimal valorVenda)
+        {
+            decimal result;
+
+            if (valorVenda <= 10000)
+            {
+                result = valorVenda * 0.05m;
+            }
+            else
+            {
+                result = valorVenda * 0.06m;
+            }
+
+            return Math.Floor(result * 100) / 100.0m;
+        }
+    }
+}
---
+++ SistemaVendas_NSubstitute/TesteVendas/TesteCalculoComissao.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaVendas;
+
+
+namespace TesteVendas
+{
+    [TestClass]
+    public class TesteCalculoComissao
+    {
+        [TestMethod]
+        public void ValidarVendaDezMil()
+        {
+            decimal valorVenda = 10000;
+            decimal valorEsperado = 500;
+
+            CalculadoraComissao calculadora = new CalculadoraComissao();
+            decimal resultado = calculadora.calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, resultado);
+        }
+
+        [TestMethod]
+        public void ValidarQueDecimalEFixe()
+        {
+            decimal valorVenda = 1;
+            decimal valorEsperado = 0.05m;
+
+            CalculadoraComissao calculadora = new CalculadoraComissao();
+            decimal resultado = calculadora.calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, resultado);
+        }
+
+        [TestMethod]
+        public void ValorMaiorQue10000()
+        {
+            decimal valorVenda = 20000;
+            decimal valorEsperado = 1200;
+
+            CalculadoraComissao calculadora = new CalculadoraComissao();
+            decimal resultado = calculadora.calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, resultado);
+        }
+
+        [TestMethod]
+        public void ValorSemPerda()
+        {
+            decimal valorVenda = 55.59m;
+            decimal valorEsperado = 2.77m;
+
+            CalculadoraComissao calculadora = new CalculadoraComissao();
+            decimal resultado = calculadora.calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, resultado);
+        }
+    }
+}
